e47afe8904c3bfd1b01d049f039298ea6cc79711
CamronUpshur <31862897+CamronUpshur@users.noreply.github.com>
2017-09-15 22:42:32 -0700

    Update Car.java

--- Drive Car Program/src/Car.java
+++ Drive Car Program/src/Car.java
@@ -1,4 +1,7 @@
-//Camron Upshur
+/*
+* Camron Upshur 
+* This program can track the fuel and creat a car
+*/
 public class Car 
 {
 	public Car(double carmpg)
@@ -6,8 +9,18 @@ public class Car
 		fuel = 0;
 		mpg = carmpg;
 	}
+	public void addgas(double gas)
+	{
+		fuel = gas;
+	}
+	public void drive(double miles)
+	{
+		fuel = (1/mpg) * miles;
+	}
+	public double fuelcheck( )
+	{
+		return fuel;
+	}
 	private double fuel;
 	private double mpg;
-	
-
 }
